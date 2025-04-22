458c7d39bd5c8fde6369a212e2ea77b79318321c
Rushikesh Mishra <113721158+rushikesh7mishra@users.noreply.github.com>
2025-04-22 11:16:34 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 // src/api.js
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5050' });
+const API = axios.create({ baseURL: 'https://ai-voice-assistant-k3d9.onrender.com' });
 
 export const askQuestion = (question) => API.post('/api/ask', { question });
